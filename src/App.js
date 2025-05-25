69ca432e5ef1c9fcc9f181d50b99743ba5249f8d
Asim Yasin <104110152+ASIMYASINOMERAHMED@users.noreply.github.com>
2025-05-25 07:54:07 -0700

    new Version

--- src/App.js
+++ src/App.js
@@ -7,6 +7,14 @@ import { useEffect, useState } from "react";
 import moment from "moment";
 import "moment/min/locales";
 import { useTranslation } from 'react-i18next';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+
 moment.locale("en");
 
 let cancelAxios= null;
@@ -15,6 +23,12 @@ function App() {
   const [lang, setLang] = useState("en");
   const [time, setTime] = useState(moment().format('MMMM Do YYYY, h:mm a'));
   const [direction, setDirection] = useState("ltr");
+  const [city, setCity] = useState("");
+  const [open, setOpen] = useState(false);
+  const [location, setLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
   const [temp, setTemp] = useState({
     temp: 0,
     description: "",
@@ -29,34 +43,35 @@ function App() {
     setTime(moment().format('Do MMMM YYYY, h:mm a'));
     setDirection(lang === "en" ? "ltr" : "rtl");
   }, []);
-  useEffect(() => {
-    axios.get("https://api.openweathermap.org/data/2.5/weather?lat=25.276987&lon=55.296249&appid=7e36852739ab69ed8f1b4b9bfb8358a2",
-      {
-        cancelToken: new axios.CancelToken((c)=>
-        {
-          cancelAxios = c;
-        }
-        )
-      }
-    )
-      .then((response) => {
-        console.log(response.data);
-        const temp = Math.round(response.data.main.temp - 272.15);
-        setTemp({
-          temp: temp,
-          description: response.data.weather[0].description,
-          minTemp: Math.round(response.data.main.temp_min - 272.15),
-          maxTemp: Math.round(response.data.main.temp_max - 272.15),
-          city: response.data.name,
-          icon: response.data.weather[0].icon,
-        });
-      })
-      .catch((error) => {
-        console.error(error);
+
+  const fetchLocationAndWeather = async () => {
+    try {
+      // Fetch location first
+      const locationResponse = await fetch("http://ip-api.com/json");
+      const locationData = await locationResponse.json();
+      const latitude = locationData.lat;
+      const longitude = locationData.lon;
+      setLocation({ latitude, longitude });
+
+      const weatherResponse = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=7e36852739ab69ed8f1b4b9bfb8358a2`
+      );
+      const tempValue = Math.round(weatherResponse.data.main.temp - 272.15);
+      setTemp({
+        temp: tempValue,
+        description: weatherResponse.data.weather[0].description,
+        minTemp: Math.round(weatherResponse.data.main.temp_min - 272.15),
+        maxTemp: Math.round(weatherResponse.data.main.temp_max - 272.15),
+        city: weatherResponse.data.name,
+        icon: weatherResponse.data.weather[0].icon,
       });
-      return () => {
-        cancelAxios();
-      };
+
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  useEffect(() => {
+   fetchLocationAndWeather();
   }, []);
   function changeLanguage() {
     if(lang === "ar")
@@ -73,8 +88,67 @@ function App() {
     setTime(moment().format('Do MMMM YYYY, h:mm a'));
     setDirection(lang === "en" ? "ltr" : "rtl");
   }
+  function findCity() {
+    const apiKey = "MAV3/YkuFOSWEBvO24833w==PgDmsCDg1vUbUZRa";
+    const weatherApiKey = "7e36852739ab69ed8f1b4b9bfb8358a2";
+    const cityName = t(city);
+  
+    axios.get(`https://api.api-ninjas.com/v1/city?name=${cityName}`, {
+      headers: { 'X-Api-Key': apiKey }
+    })
+    .then(response => {
+      if (!response.data.length) setOpen(true);
+      const { latitude: lat, longitude: lon } = response.data[0];
+    
+      return axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`);
+    })
+    .then(response => {
+      const { temp, temp_min, temp_max } = response.data.main;
+      setTemp({
+        temp: Math.round(temp - 272.15),
+        description: response.data.weather[0].description,
+        minTemp: Math.round(temp_min - 272.15),
+        maxTemp: Math.round(temp_max - 272.15),
+        city: response.data.name,
+        icon: response.data.weather[0].icon
+      });
+    })
+    .catch(error => console.error("Error fetching data:", error.message));
+  }
+
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  
+
   return (
-    <div>
+    <>
+{/*Dialog*/}
+
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Error"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            City not found, please try again.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} autoFocus>
+            Ok
+          </Button>
+        </DialogActions>
+      </Dialog>
+  
+{/*Dialog*/}
+    <div>  
       <Container maxWidth="sm" style={{ padding: "20px" }}>
         <div
           style={{
@@ -85,6 +159,21 @@ function App() {
             alignItems: "center",
           }}
         >
+          {/*Header*/}
+          <div style={{width: "100%", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+            <Typography variant="h2" style={{color: "white", fontWeight: "bold"}}>
+              {t("Weather App")}
+            </Typography>
+            {/*Search*/}
+            <div dir={direction} style={{display: "flex", justifyContent: "center", alignItems: "center", width: "80%"}}>
+            <TextField value={city} onChange={(e) => setCity(e.target.value)} color="primary" id="outlined-basic" label={t("Search city")} variant="outlined" style={{margin: "20px",width: "100%"}}/>
+            <Button disabled={city === ""} variant="contained" color="primary" style={{margin: "20px", height: "50px"}} onClick={() => findCity()}>
+              {t("Search")}
+            </Button>
+            </div>
+            {/*Search*/}
+          </div>
+          {/*Header*/}
           {/*Card*/}
           <div 
           dir={direction}
@@ -172,6 +261,7 @@ function App() {
         </div>
       </Container>
     </div>
+    </>
   );
 }
 
